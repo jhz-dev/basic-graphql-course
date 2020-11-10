8ce405bb647e14513c5e96758dee047da37bf813
Jonathan Hernandez <jonathan.udemgmail.com>
2020-11-09 21:46:40 -0500

    backup

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+require('dotenv').config()
+const { makeExecutableSchema } = require('graphql-tools')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+const cors = require('cors')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+
+const resolvers = require('./lib/resolvers')
+
+const app = express()
+const port = process.env.port || 3000
+const isDev = process.env.NODE_ENV !== 'production'
+
+// schema
+const typeDefs = readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
+)
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+app.use(cors());
+
+app.use('/api', gqlMiddleware({
+  schema,
+  rootValue: resolvers,
+  graphiql: isDev
+}))
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`)
+})
